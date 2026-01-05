63fe329843b29533a98070ed715a271f1a63a352
llhsdmd <2982886437@qq.com>
2026-01-05 18:03:04 +0800

    [fix] update ilias version

--- xmake.lua
+++ xmake.lua
@@ -11,10 +11,10 @@ add_configfiles("include/ilias/sql/global/config.h.in")
 set_configdir("include/ilias/sql/global")
 set_configvar("API_VERSION", 1)
 
-add_requires("ilias", {version = "0.3.2", configs = {log = true, cpp20 = true}})
+add_requires("ilias", {version = "0.3.3", configs = {log = true, cpp20 = true}})
 add_requires("neko-proto-tools", {version = "dev", configs = {shared = is_config("3rd_kind", "shared"), enable_rapidxml = false, enable_simdjson = false, enable_protocol = false, enable_rapidjson = false, enable_fmt = false, enable_communication = false}})
 
-add_requireconfs("**.ilias", {version = "0.3.2", override = true, configs = {log = true, cpp20 = true}})
+add_requireconfs("**.ilias", {version = "0.3.3", override = true, configs = {log = true, cpp20 = true}})
 add_requireconfs("**.neko-proto-tools", {override = true, version = "dev", configs = {shared = is_config("3rd_kind", "shared"), enable_rapidxml = false, enable_simdjson = false, enable_protocol = false, enable_rapidjson = false, enable_fmt = false, enable_communication = false}})
 
 includes("lua/hidetargets.lua")
